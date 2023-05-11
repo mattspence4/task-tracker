3673ddebe1ad48b8c967b593fad5137f306a3048
mattspence4 <113352659+mattspence4@users.noreply.github.com>
2023-05-11 15:01:20 -0400

    added delete task functionality

--- task/src/App.js
+++ task/src/App.js
@@ -35,7 +35,9 @@ const addTask = (task) => {
 }
 
 //Delete task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
+
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
