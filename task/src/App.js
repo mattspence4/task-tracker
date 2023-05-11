aa0b54cb37f4e2d9212586040bb33ed3206b269a
mattspence4 <113352659+mattspence4@users.noreply.github.com>
2023-05-11 13:56:30 -0400

    implemented toggling reminders and adding tasks
    *also removed some unnecessary testing code

--- task/src/App.js
+++ task/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import {useState} from 'react'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
     {
@@ -26,19 +28,30 @@ const App = () => {
     },
 ])
 
-//Delete task
-const deleteTask = (id) => {
-  const foo = tasks.filter((task) => task.id !== id)
-  setTasks(foo)
-  
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000)+1
+
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
 }
 
-//Add task
+//Delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id == id ? {...task, reminder: !task.reminder} : task))
+}
+
+//toggle reminder
 
   return (
     <div className="container">
-      <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks Remaining'}
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks Remaining'}
     </div>
   )
 }
