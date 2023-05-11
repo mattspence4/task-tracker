493e8f34e41363a1683950b97ff5c21399a3d682
mattspence4 <113352659+mattspence4@users.noreply.github.com>
2023-05-11 15:31:10 -0400

    connected reminder toggling to backend

--- task/src/App.js
+++ task/src/App.js
@@ -25,6 +25,14 @@ const App = () => {
     return data
   }
 
+  //fetch task
+  const fetchTask = async (id) => {
+    const res = await  fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
 
 // Add Task
 const addTask = async (task) => {
@@ -52,11 +60,21 @@ const deleteTask = async (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id == id ? {...task, reminder: !task.reminder} : task))
-}
-
 //toggle reminder
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {method: 'PUT', headers: {
+    'Content-type': 'application/json'
+  },
+  body: JSON.stringify(updatedTask)
+  })
+
+  const data = await res.json()
+
+  setTasks(tasks.map((task) => task.id == id ? {...task, reminder: data.reminder} : task))
+}
 
   return (
     <div className="container">
